24f06262365a991b7ad280044ea94e20681cd08d
vanessasousapro <vanessasousa.pro@gmail.com>
2025-08-13 14:27:33 -0300

    Implement prime number checker program

---
+++ java/JavaAcademicExercise/src/main/java/org/vanessa/javaacademicexercise/JavaAcademicExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.vanessa.javaacademicexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAcademicExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAcademicExerciseApplication.class, args);
+	}
+
+}
---
+++ java/JavaAcademicExercise/src/test/java/org/vanessa/javaacademicexercise/JavaAcademicExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vanessa.javaacademicexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAcademicExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
