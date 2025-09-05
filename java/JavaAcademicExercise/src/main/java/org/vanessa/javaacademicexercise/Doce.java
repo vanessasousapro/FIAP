b19e47379481887f20986de29c8710e33f9a25c5
vanessasousapro <vanessasousa.pro@gmail.com>
2025-09-05 13:28:34 -0300

    feat: add attack/energy methods a,pecial ability;  andxtend Menu

---
+++ java/JavaAcademicExercise/src/main/java/org/vanessa/javaacademicexercise/Doce.java
@@ -0,0 +1,9 @@
+package org.vanessa.javaacademicexercise;
+
+public class Doce {
+    String nome;
+
+    public void dizerNome() {
+        System.out.println("Nome do Doce: " + nome);
+    }
+}
--- java/JavaAcademicExercise/src/main/java/org/vanessa/javaacademicexercise/PersonagemMagico.java
+++ java/JavaAcademicExercise/src/main/java/org/vanessa/javaacademicexercise/PersonagemMagico.java
@@ -1,13 +1,13 @@
 package org.vanessa.javaacademicexercise;
 
 public class PersonagemMagico {
-
+    //Atributos
     String nome;
     int nivelEnergia;
     String poderMagico;
     HabilidadeEspecial habilidadeEspecial;
 
-
+    //Construtores
     public PersonagemMagico(String nome, int nivelEnergia, String poderMagico) {
         this.nome = nome;
         this.nivelEnergia = nivelEnergia;
@@ -20,5 +20,30 @@ public class PersonagemMagico {
 
     public PersonagemMagico() {
 
+    }
+    //Métodos
+    public void atacar(String ataque) {
+        if(nivelEnergia >= 10) {
+            System.out.println(nome + "Realizou um ataque: " + ataque);
+            nivelEnergia -= 10;
+        } else {
+            System.out.println(nome + " Esta sem energia para atacar!");
+        }
+    }
+
+    public int aumentarEnergia(int energia) {
+        nivelEnergia += energia;
+        return nivelEnergia;
+    }
+
+    public void ativarHabilidadeEspecial() {
+        if (!habilidadeEspecial.habilitada) {
+            System.out.println("Habilidade especial não está habilitada!");
+        } else if (nivelEnergia >= habilidadeEspecial.custoEnergia){
+            System.out.println("Ativando habilidade: " + habilidadeEspecial.nome);
+            nivelEnergia -= habilidadeEspecial.custoEnergia;
+        } else {
+            System.out.println(nome + " Está sem energia para atacar!");
+        }
     }
 }
