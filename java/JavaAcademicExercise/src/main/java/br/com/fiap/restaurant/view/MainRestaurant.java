a535680c766d64858d835c186e20a5fd1189a89b
vanessasousapro <vanessasousa.pro@gmail.com>
2025-09-12 15:01:05 -0300

    feature: add inheritance models

---
+++ java/JavaAcademicExercise/src/main/java/br/com/fiap/restaurant/view/MainRestaurant.java
@@ -0,0 +1,26 @@
+package br.com.fiap.restaurant.view;
+
+import br.com.fiap.restaurant.model.Bebida;
+import br.com.fiap.restaurant.model.PratoQuente;
+
+public class MainRestaurant {
+    public static void main(String[] args) {
+        Bebida bebida = new Bebida("Suco de Laranja", "Àgua, Laranja, Açucar, Gelo", 10.00, 500);
+        System.out.println("Bebida");
+        System.out.println("A Bebida Informada foi: " + bebida.getNome());
+        System.out.println("A Bebida é Composta por: " + bebida.getIngredientes());
+        System.out.println("E custa R$ " + bebida.getPreco());
+        System.out.println("E a sua quantidade é de: " + bebida.getQuantidade() + "ml");
+
+        PratoQuente prato = new PratoQuente();
+        prato.setNome("Omelete");
+        prato.setIngredientes("Ovo, Tomate, Cebola, e Queijo");
+        prato.setPreco(15.00);
+        prato.setServe(2);
+        System.out.println("Prato");
+        System.out.println("O prato informado foi: " + prato.getNome());
+        System.out.println("É comporto por: " + prato.getIngredientes());
+        System.out.println("E custa R$ " + prato.getPreco());
+        System.out.println("E serve " + prato.getServe() + " pessoas");
+    }
+}
